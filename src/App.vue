0e386738c532ef68a7c2ec825af259cacc01167c
iseungjun <kyyeto9984@gmail.com>
2025-08-25 23:41:41 +0900

    vue cli

--- src/App.vue
+++ src/App.vue
@@ -8,23 +8,23 @@
 import AppHeader from './components/AppHeader.vue';
 
 export default {
-  data : function () {
+  data : function () { // 변수등이 들어갈 곳...
      return {
         str : "Header!!~",
         surfix: ""
      }
   },
-  computed: {
+  computed: { // data를 기반으로 validation이나... kotlin의 get property 등.. 추가작업을 해야할 떄 사용하는듯
     computedStr: function () {
        return this.str + this.surfix
     }
   },
-  methods: {
+  methods: { // 함수들을 여기에 선언
     renewCount: function(v) {
       this.surfix = v
     }
-  },
-  components: {
+  }, 
+  components: { // 붙일 컴포넌트들을 여기에 씀
     AppHeader
   }
 }
---
+++ src/components/a.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- html.. -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    // Javascript...
+    data: {
+
+    },
+    methods: {
+
+    },
+    computed: {
+        
+    },
+    watch: {
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
